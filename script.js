a139f9b5c94f15d686d5e1ea1ed1bc9185b8ef2f
DannyKaziu <128734634+DannyKaziu@users.noreply.github.com>
2023-03-23 14:58:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const htmlEditor = document.getElementById("htmlEditor");
-const cssEditor = document.getElementById("cssEditor");
-const jsEditor = document.getElementById("jsEditor");
+const htmlEditor = ace.edit("htmlEditor");
+const cssEditor = ace.edit("cssEditor");
+const jsEditor = ace.edit("jsEditor");
 const preview = document.getElementById("preview");
 
+htmlEditor.setTheme("ace/theme/monokai");
+cssEditor.setTheme("ace/theme/monokai");
+jsEditor.setTheme("ace/theme/monokai");
+
+htmlEditor.getSession().setMode("ace/mode/html");
+cssEditor.getSession().setMode("ace/mode/css");
+jsEditor.getSession().setMode("ace/mode/javascript");
+
 function updatePreview() {
-  const htmlCode = htmlEditor.value;
-  const cssCode = `<style>${cssEditor.value}</style>`;
-  const jsCode = `<script>${jsEditor.value}<\/script>`;
+  const htmlCode = htmlEditor.getValue();
+  const cssCode = `<style>${cssEditor.getValue()}</style>`;
+  const jsCode = `<script>${jsEditor.getValue()}<\/script>`;
   preview.contentWindow.document.open();
   preview.contentWindow.document.write(htmlCode + cssCode + jsCode);
   preview.contentWindow.document.close();
 }
 
-htmlEditor.addEventListener("input", updatePreview);
-cssEditor.addEventListener("input", updatePreview);
-jsEditor.addEventListener("input", updatePreview);
+htmlEditor.getSession().on("change", updatePreview);
+cssEditor.getSession().on("change", updatePreview);
+jsEditor.getSession().on("change", updatePreview);
